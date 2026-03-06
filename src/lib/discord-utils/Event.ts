bcc77ef4563279862acf66377fc4f52757aff6f2
Kyvrixon <ky@kyvrixon.dev>
2026-03-06 12:59:25 +1100

    Added custom type generics for autocomplete

--- src/lib/discord-utils/Event.ts
+++ src/lib/discord-utils/Event.ts
@@ -4,36 +4,47 @@ import type { Client, ClientEvents, RestEvents } from "discord.js";
 type EventType = "client" | "rest" | "custom";
 
 export class DiscordEvent<
-	T extends EventType,
-	K extends T extends "custom"
-		? string
-		: keyof (T extends "client" ? ClientEvents : RestEvents),
-	V extends Client,
+	C extends Record<string, any[]> = {
+		"you forgot to pass in your types!": []
+	},
+	V extends Client = Client,
+	T extends EventType = EventType,
 > {
 	public readonly type: T;
-	public readonly name: K;
+	public readonly name: string;
 	public readonly once: boolean;
-	public readonly method: (
-		client: V,
-		...args: T extends "client"
-			? K extends keyof ClientEvents
-				? ClientEvents[K]
-				: any[]
-			: T extends "rest"
-				? K extends keyof RestEvents
-					? RestEvents[K]
-					: any[]
-				: any[]
-	) => Promise<void>;
+	public readonly method: (client: V, ...args: any[]) => Promise<void>;
 
-	constructor(ops: {
-		type: DiscordEvent<T, K, V>["type"];
-		name: DiscordEvent<T, K, V>["name"];
-		once: DiscordEvent<T, K, V>["once"];
-		method: DiscordEvent<T, K, V>["method"];
-	}) {
-		this.type = ops.type;
-		this.name = ops.name;
+	constructor(
+		ops: T extends "custom"
+			? {
+					type: "custom";
+					name: keyof C;
+					once: boolean;
+					method: (client: V, ...args: C[keyof C]) => Promise<void>;
+				}
+			: T extends "client"
+				? {
+						type: "client";
+						name: keyof ClientEvents;
+						once: boolean;
+						method: (
+							client: V,
+							...args: ClientEvents[keyof ClientEvents]
+						) => Promise<void>;
+					}
+				: {
+						type: "rest";
+						name: keyof RestEvents;
+						once: boolean;
+						method: (
+							client: V,
+							...args: RestEvents[keyof RestEvents]
+						) => Promise<void>;
+					},
+	) {
+		this.type = ops.type as T;
+		this.name = String(ops.name);
 		this.once = ops.once;
 		this.method = ops.method;
 	}

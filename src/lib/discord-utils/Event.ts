6fa4935d6a0550ffbedc8145bca0c4253302d073
Kyvrixon <ky@kyvrixon.dev>
2026-03-06 13:25:27 +1100

    typing fix

--- src/lib/discord-utils/Event.ts
+++ src/lib/discord-utils/Event.ts
@@ -2,49 +2,43 @@
 import type { Client, ClientEvents, RestEvents } from "discord.js";
 
 type EventType = "client" | "rest" | "custom";
+type DefaultTyping = { "You forgot to pass in your types to the class!": [] };
 
 export class DiscordEvent<
-	C extends Record<string, any[]> = {
-		"you forgot to pass in your types!": []
-	},
 	V extends Client = Client,
-	T extends EventType = EventType,
+	C extends Record<string, any[]> = DefaultTyping,
+	T extends EventType = "custom",
+	K extends
+		| keyof C
+		| keyof ClientEvents
+		| keyof RestEvents = keyof DefaultTyping,
 > {
 	public readonly type: T;
-	public readonly name: string;
+	public readonly name: K;
 	public readonly once: boolean;
-	public readonly method: (client: V, ...args: any[]) => Promise<void>;
+	public readonly method: (
+		client: V,
+		...args: T extends "client"
+			? K extends keyof ClientEvents
+				? ClientEvents[K]
+				: any[]
+			: T extends "rest"
+				? K extends keyof RestEvents
+					? RestEvents[K]
+					: any[]
+				: K extends keyof C
+					? C[K]
+					: any[]
+	) => Promise<void>;
 
-	constructor(
-		ops: T extends "custom"
-			? {
-					type: "custom";
-					name: keyof C;
-					once: boolean;
-					method: (client: V, ...args: C[keyof C]) => Promise<void>;
-				}
-			: T extends "client"
-				? {
-						type: "client";
-						name: keyof ClientEvents;
-						once: boolean;
-						method: (
-							client: V,
-							...args: ClientEvents[keyof ClientEvents]
-						) => Promise<void>;
-					}
-				: {
-						type: "rest";
-						name: keyof RestEvents;
-						once: boolean;
-						method: (
-							client: V,
-							...args: RestEvents[keyof RestEvents]
-						) => Promise<void>;
-					},
-	) {
-		this.type = ops.type as T;
-		this.name = String(ops.name);
+	constructor(ops: {
+		type: T;
+		name: K;
+		once: boolean;
+		method: DiscordEvent<V, C, T, K>["method"];
+	}) {
+		this.type = ops.type;
+		this.name = ops.name;
 		this.once = ops.once;
 		this.method = ops.method;
 	}

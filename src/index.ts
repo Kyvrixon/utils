672684cb4740ff02aede417375b574077b80818a
Kyvrixon <ky@kyvrixon.dev>
2026-03-09 10:11:02 +1100

    added createPagination.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./lib/formatSeconds";
 export * from "./lib/toOrdinal";
 export * from "./lib/discord-utils/Command";
+export * from "./lib/discord-utils/createPagination";
 export * from "./lib/discord-utils/Event";
 export * from "./lib/modules/LoggerModule";

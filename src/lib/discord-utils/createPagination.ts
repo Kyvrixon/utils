992848678d76763502c2d583320a371988a6cc0d
Kyvrixon <ky@kyvrixon.dev>
2026-03-09 11:10:16 +1100

    added allowedmentions to responses

--- src/lib/discord-utils/createPagination.ts
+++ src/lib/discord-utils/createPagination.ts
@@ -128,7 +128,11 @@ export async function createPagination(
 
 	if (!list.length) {
 		await interaction
-			.followUp({
+			.reply({
+				allowedMentions: {
+					parse: [],
+					repliedUser: false,
+				},
 				components: [
 					new ContainerBuilder().addTextDisplayComponents(
 						new TextDisplayBuilder().setContent("No data to show"),
@@ -149,7 +153,7 @@ export async function createPagination(
 			lastPage.map((comp) => {
 				if (comp.type === "display") {
 					return {
-						type: "display" as const,
+						type: "display",
 						component: new TextDisplayBuilder(comp.component.toJSON()),
 					};
 				}
@@ -231,6 +235,10 @@ export async function createPagination(
 					generateContainer(Math.floor(currentIndex / entriesPerPage)),
 				],
 				flags: ["IsComponentsV2"],
+				allowedMentions: {
+					parse: [],
+					repliedUser: false,
+				},
 			});
 		} catch (error) {
 			const e = error as Error;
@@ -287,7 +295,7 @@ export async function createPagination(
 				await btn
 					.followUp({
 						content: "Modal timed out.",
-						flags: MessageFlags.Ephemeral,
+						flags: ["Ephemeral"],
 					})
 					.catch(() => null);
 				return;
@@ -307,6 +315,10 @@ export async function createPagination(
 					.reply({
 						content: `Invalid page! Choose a number between **1** and **${totalPages}**.`,
 						flags: ["Ephemeral"],
+						allowedMentions: {
+							parse: [],
+							repliedUser: false
+						}
 					})
 					.catch(() => null);
 				return;
